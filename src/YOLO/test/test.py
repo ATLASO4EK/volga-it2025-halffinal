3a8721bcf470c2ec022370ca2f0785c5fb6dea47
AtLaS <atlaso4ek@gmail.com>
2025-10-05 21:33:33 +0700

    YOLO-test

---
+++ src/YOLO/test/test.py
@@ -0,0 +1,20 @@
+import os
+from ultralytics import YOLO
+
+root = 'D:/pythonic-shit/volga-it2025-halffinal/src/YOLO/test'
+os.chdir(root)
+
+model = YOLO('best.pt')
+
+os.chdir(f'{root}/dataset')
+
+results = model(os.listdir())  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
